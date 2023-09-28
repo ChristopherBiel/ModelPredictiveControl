eaebaf469fa35225d7a327f9e11dc93518d8b177
Christopher Biel <christopher.biel@biels.net>
2023-09-28 11:04:27 +0200

    Renamed Folder, reverted gitignore for that change

---
+++ ProjectAssignment/user_settings.py
@@ -0,0 +1,4 @@
+# User specific settings - file changes are ignored by git
+
+trajectory_quat = '/Users/chris/Documents/_MASTER/ModelPredictiveControl/MPC_Assignments/Project Assignment/Dataset/trajectory_quat.txt'
+tuning_file_path = '/Users/chris/Documents/_MASTER/ModelPredictiveControl/MPC_Assignments/Project Assignment/tuning.yaml'
